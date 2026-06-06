f8962c4690063db3120c3b8af7f30f895dd6657c
victor_yema <vi.urrav@duocuc.cl>
2026-06-06 10:34:48 -0400

    Se crea ComentariosModelAssembler para soporte de enlaces hipermedia HATEOA

--- src/main/java/com/HomeRentSolution/ms_comentarios/assemblers/ComentariosModelAssembler.java
+++ src/main/java/com/HomeRentSolution/ms_comentarios/assemblers/ComentariosModelAssembler.java
@@ -1,4 +1,23 @@
 package com.HomeRentSolution.ms_comentarios.assemblers;
 
-public class ComentariosModelAssembler {
+import com.HomeRentSolution.ms_comentarios.controller.ComentariosController;
+import com.HomeRentSolution.ms_comentarios.dto.ComentariosResponseDTO;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class ComentariosModelAssembler implements RepresentationModelAssembler<ComentariosResponseDTO, EntityModel<ComentariosResponseDTO>> {
+
+    @Override
+    public EntityModel<ComentariosResponseDTO> toModel (ComentariosResponseDTO dto){
+
+        return EntityModel.of(dto,
+                linkTo(methodOn(ComentariosController.class).getPorId(dto.getIdComentario())).withSelfRel(),
+                linkTo(methodOn(ComentariosController.class).getComentarios()).withRel("comentarios")
+                );
+    }
 }
