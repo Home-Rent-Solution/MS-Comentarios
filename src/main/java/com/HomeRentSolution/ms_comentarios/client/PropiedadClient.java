76a256e790c4445ac48544617ef020ce7e066ffe
catbarriosg <catbarriosg@duocuc.cl>
2026-06-21 17:41:16 -0400

    Corregir rutas Feign y timeouts en MS-Comentarios

--- src/main/java/com/HomeRentSolution/ms_comentarios/client/PropiedadClient.java
+++ src/main/java/com/HomeRentSolution/ms_comentarios/client/PropiedadClient.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
-        name = "propiedad-client"
+        name = "ms-propiedades",
+        url = "${ms.propiedades.url}"
 )
 public interface PropiedadClient {
 
