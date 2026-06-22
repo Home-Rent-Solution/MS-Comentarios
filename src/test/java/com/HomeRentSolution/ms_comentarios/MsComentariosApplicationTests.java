d1ee04b40fce3768b098ee8455ada57302882671
catbarriosg <catbarriosg@duocuc.cl>
2026-06-21 20:21:28 -0400

    Corrige configuracion test y YAML de comentarios

--- src/test/java/com/HomeRentSolution/ms_comentarios/MsComentariosApplicationTests.java
+++ src/test/java/com/HomeRentSolution/ms_comentarios/MsComentariosApplicationTests.java
@@ -2,12 +2,14 @@ package com.HomeRentSolution.ms_comentarios;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MsComentariosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
